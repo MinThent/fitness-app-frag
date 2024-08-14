1da21c9b0045920cfc3280cfbdb7c8b18c3d9186
airfrag <80678006+MinThent@users.noreply.github.com>
2024-08-14 21:04:37 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fitness-app
+ a fitness app made by following the yt tutorial of James McArthur
---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function Hero() {
+    return (
+        <div className='min-h-screen flex flex-col gap-10 items-center 
+        justify-center text-center max-w-[800px] w-full mx-auto p-4'>
+            <div className='flex flex-col gap-4'>
+                <p>
+                    IT'S TIME TO GET
+                </p>
+            </div>
+            <h1 className='uppercase 
+            font-semibold text-4xl sm:text-5xl 
+            md:text-6xl lg:text-7xl'>
+                Enor<span className='text-blue-400'>mous</span>
+            </h1>
+            <p className='text-sm md:text-base font-light'>
+                I hereby acknowledgement
+                that I may become
+                <span className='text-blue-400 font-medium'> unbelievably big big big </span>
+                and accept all risks of becoming the local
+                <span className='text-blue-400 font-medium'> mass montrosity</span>,
+                afflicted with severe body dismorphia, unable to fit through doors
+            </p>
+            <button className='px-8 py-4 rounded-md border-[2px]
+             border-blue-400 border-solid bg-slate-950 blueShadow duration-200'>
+                <p>Accept & Begin</p>
+            </button>
+        </div>
+    )
+}
